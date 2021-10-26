9e1fca067ad1712a0e310f114e5d888ff6476367
Ben Miller <benm4643@gmail.com>
2021-10-26 17:04:35 -0400

    Project Initiation
    Creating Base Project

---
+++ FhotoFithFriends/src/main/java/mille5bm/uc/edu/FhotoFithFriends/FhotoFithFriendsApplication.java
@@ -0,0 +1,13 @@
+package mille5bm.uc.edu.FhotoFithFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhotoFithFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhotoFithFriendsApplication.class, args);
+	}
+
+}
---
+++ FhotoFithFriends/src/test/java/mille5bm/uc/edu/FhotoFithFriends/FhotoFithFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package mille5bm.uc.edu.FhotoFithFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhotoFithFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
