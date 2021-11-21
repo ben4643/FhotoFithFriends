39180f7f8c59ca13f45b97c3635ba4a780007d64
sharmrb <57459468+sharmrb@users.noreply.github.com>
2021-11-21 16:23:26 -0500

    Added blank Service , Dao and entity packages for better architechture

---
+++ FhotoFithFriends/src/main/java/mille5bm/uc/edu/FhotoFithFriends/dao/FhotoRepository.java
@@ -0,0 +1,9 @@
+package mille5bm.uc.edu.FhotoFithFriends.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public class FhotoRepository extends JpaRepository <Fhoto, Integer> {
+
+}
