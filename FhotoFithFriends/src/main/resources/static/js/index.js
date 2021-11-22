5c498516936b2450b3b45c11922d0cbc7d632305
provanpm <provanpm@mail.uc.edu>
2021-11-21 20:32:26 -0500

    Accessing and capturing video with browser Web API
    Also deleted html in wrong place

--- FhotoFithFriends/src/main/resources/static/js/index.js
+++ FhotoFithFriends/src/main/resources/static/js/index.js
@@ -1,3 +1,19 @@
-// Elements
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
 
-// This is where your Camera and other API logic can go
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
